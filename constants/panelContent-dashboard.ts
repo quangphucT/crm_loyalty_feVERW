05b4134f286985f5b00530d58dc1d94222e94cd6
unknown <phucp9698@gmail.com>
2026-04-04 10:55:07 +0700

    building content  chatBox

--- constants/panelContent-dashboard.ts
+++ constants/panelContent-dashboard.ts
@@ -2,8 +2,8 @@ import { PanelContent } from "@/types/panelContent.type";
 
 export const panelContent: Record<string, PanelContent> = {
   customers: {
-    title: "Quản lý khách hàng",
-    subtitle: "Quản lý khách hàng & điểm tích luỹ",
+    title: "Quản lý khách hàng123",
+    subtitle: "Quản lý khách hàng & điểm tích luỹ123",
     description:
       "Danh sách khách hàng và thông tin điểm tích lũy để quản lý nhanh chóng.",
   }
